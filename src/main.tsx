5d99b82fff550f21ba4da345e1ceb44066b5316b
AKareem2005 <abdulkareemmohammed2005@gmail.com>
2026-05-14 22:34:34 +0530

    restore full aviation website

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createRouter,
+} from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
