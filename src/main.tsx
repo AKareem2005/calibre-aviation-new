c7b09f5cb697047e031876d2804c19ac34450c06
AKareem2005 <abdulkareemmohammed2005@gmail.com>
2026-05-14 20:24:05 +0530

    restore full aviation website

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-  return (
-    <div>
-      <h1>Calibre Aviation Academy</h1>
-    </div>
-  );
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
